97f5906e40bc7c2e40998715a9d5fe27c59b69f9
Michael Enderli <146711963+LaTartaRugaa@users.noreply.github.com>
2023-12-03 21:04:02 +0100

    Add files via upload

---
+++ Code/Raspberry Pi Zero W/credentials.py
@@ -0,0 +1,8 @@
+github_username = "LaTartaRugaa"
+github_repository = "Lastmanagement-System"
+github_db_file_path = "db.json"
+github_log_file_path = "data_log.json"
+github_access_email = ""
+github_token = ""
+wattpilot_password = ""
+wattpilot_ip = ""
